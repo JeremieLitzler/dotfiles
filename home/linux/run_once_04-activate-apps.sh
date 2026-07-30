e44aada25eb3760034bf9a2e6b9b4bef4ca5f160
Jérémie LITZLER <2410556+JeremieLitzler@users.noreply.github.com>
2026-07-30 17:56:36 +0200

    Update run_once_04-activate-apps.sh

--- home/linux/run_once_04-activate-apps.sh
+++ home/linux/run_once_04-activate-apps.sh
@@ -1,8 +1,8 @@
 if ! grep -q 'carapace' ~/.bashrc; then
     echo "export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense' # optional" >> ~/.bashrc
-    echo "source <(carapace _carapace)" >> bashrc
+    echo "source <(carapace _carapace)" >> ~/.bashrc
 fi
 
 if ! grep -q 'atuin' ~/.bashrc; then
-    eval "$(atuin init bash)"
+    echo 'eval "$(atuin init bash)"'  >> ~/.bashrc
 fi
